e3ab9cf687067e843776286d87701a50bab99178
Jason <jcox@winsor.edu>
2019-11-14 10:25:58 -0500

    Update RefrigeratorThermostat.ino

--- RefrigeratorThermostat.ino
+++ RefrigeratorThermostat.ino
@@ -3,19 +3,17 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// Data wire is plugged into port 2 on the Arduino
+// DS18S20 is plugged into port 2
 #define ONE_WIRE_BUS 2
 
 
-#define LOW_END 30
-#define HIGH_END 45
+#define LOW_END 30  //degrees F
+#define HIGH_END 45 //degrees F
 
-float currentTarget = 36;
+#define POT A0      // potentiometer for setting target
+#define RELAY 10    // relay for the compressor
 
-#define POT A0
-#define RELAY 10
-
-#define DELTA_T 3
+#define DELTA_T 3   // cycle the compressor at target + or - DELTA_T
 
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
@@ -26,22 +24,31 @@ DallasTemperature sensors(&oneWire);
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-
+// take the average temperature over the past 50 readings to smooth out any anomalous readings.
 float temperature[50];
 float average = 0;
 int count = 0;
 
-//float target[5];
-float targavg = 0;
+float target = 36;  //target temperature
 
 bool isOn = false;
 
+// because the built in map function returns an int
 float map_float(float x, float a, float b, float A, float B)
 {
   float m = (B-A)/(b-a);
   float y = (x - a)*m + A;
 
   return y;
+}
+
+// map to target range with 0.25 precision.
+float map_25inc(int x, int a, int b, int A, int B)
+{
+   x = map(x, a, b, A*4, B*4);
+   float y = (float)x / 4.0;
+
+   return y;
 }
 
 void setup()
@@ -62,13 +69,14 @@ void setup()
   {
     sensors.requestTemperatures();
     temperature[i] = sensors.getTempFByIndex(0);
-    float aread = analogRead(POT);
-    //target[i%5] = map_float(aread, 0, 1023, LOW_END, HIGH_END);
     delay(5);
   }
+
+  updateAverage();
   
 }
 
+// 
 void updateAverage()
 {
   float sum = 0;
@@ -76,46 +84,45 @@ void updateAverage()
   for(int i = 0; i < 50; i++)
   {
     sum += temperature[i];
-    //sum2 += target[i%5];
   }
 
   average = sum / 50;
-  //targavg = sum2 / 50;
 }
 
 void loop()
 {
   sensors.requestTemperatures();
-  //lcd.clear();
 
   temperature[count] = sensors.getTempFByIndex(0);
-  
-  float aread = analogRead(POT);
-  targavg = map_float(aread, 0, 1023, LOW_END, HIGH_END);
   count++;
   if(count==50) count = 0;
-
+  
   updateAverage();
-
-  if(average > targavg + DELTA_T && !isOn)
+  
+  int aread = analogRead(POT);
+  target = map_25inc(aread, 0, 1023, LOW_END, HIGH_END);
+  
+  if(average > target + DELTA_T && !isOn)
   {
     digitalWrite(RELAY, HIGH);
     isOn = true;
   }
 
-  if(average <= targavg - DELTA_T && isOn)
+  if(average <= target - DELTA_T && isOn)
   {
     digitalWrite(RELAY, LOW);
     isOn = false;
   }
-  //lcd.clear();
+
+  
   lcd.setCursor(0,0);
   lcd.print("Temp: ");
   lcd.print(average);
   lcd.print(" F");
   lcd.setCursor(0,1);
   lcd.print("Target: ");
-  lcd.print(targavg);
+  lcd.print(target);
   lcd.print(" F");
+  
   delay(20);
 }
