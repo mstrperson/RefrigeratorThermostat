008a317f4040968f3edf8488c12f45a0d7228dfd
Jason <jcox@winsor.edu>
2019-11-18 13:53:00 -0500

    Update RefrigeratorThermostat.ino

--- RefrigeratorThermostat.ino
+++ RefrigeratorThermostat.ino
@@ -3,7 +3,13 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <Thread.h>
+#include <RotaryEncoder.h>
 
+#define DEBUG 0
+
+#define encCLK 4
+#define encDT  5
+#define ecnSW  6
 
 // DS18S20 is plugged into port 2
 #define ONE_WIRE_BUS 2
@@ -15,11 +21,20 @@
 #define RELAY 10    // relay for the compressor
 
 #define DELTA_T 3   // cycle the compressor at target + or - DELTA_T
+#define DEFAULT_TEMP 36.0
 
+RotaryEncoder encoder(5, 4);
+//int pos;
+
+Thread tempReadThread = Thread();
+#define TEMP_UPDATE 30000 // 30 seconds
 
 Thread displayCycle = Thread();
 int displayIndex = 0;
 #define CYCLE_INTERVAL 2000
+
+Thread updateSecondLine = Thread();
+#define SECOND_UPDATE 250
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
@@ -36,7 +51,7 @@ DeviceAddress topThermometer   = { 0x28, 0xAA, 0x7D, 0x13, 0x55, 0x14, 0x01, 0x7
 float topTemperature;
 float bottomTemperature;
 float avgTemp;
-float target = 36;  //target temperature
+float target = DEFAULT_TEMP;  //target temperature
 
 bool isOn = false;
 
@@ -60,6 +75,9 @@ float map_25inc(int x, int a, int b, int A, int B)
 
 void setup()
 {
+#if DEBUG
+  Serial.begin(9600);
+#endif
   lcd.init();  
   // Print a message to the LCD.
   lcd.backlight();
@@ -67,13 +85,52 @@ void setup()
   lcd.print("Initializing");
   lcd.setCursor(1,1);
   lcd.print("Please wait...");
-
+  
   pinMode(RELAY, OUTPUT);
   
   sensors.begin();
 
+  readThermometers();
+
   displayCycle.onRun(cycleDisplay);
   displayCycle.setInterval(CYCLE_INTERVAL);
+
+  tempReadThread.onRun(readThermometers);
+  tempReadThread.setInterval(TEMP_UPDATE);
+
+  updateSecondLine.onRun(updateTargetLCD);
+  updateSecondLine.setInterval(SECOND_UPDATE);
+}
+
+void updateTargetLCD()
+{
+  lcd.setCursor(0,1);
+  lcd.print("Target: ");
+  lcd.print(target);
+  lcd.print(" F");
+}
+
+void readThermometers()
+{
+  sensors.requestTemperatures();
+  
+  topTemperature = sensors.getTempF(topThermometer);
+  bottomTemperature = sensors.getTempF(bottomThermometer);
+  avgTemp = topTemperature + bottomTemperature;
+  avgTemp /= 2;
+  
+  
+  if(avgTemp > target + DELTA_T && !isOn)
+  {
+    digitalWrite(RELAY, HIGH);
+    isOn = true;
+  }
+
+  if((avgTemp <= target - DELTA_T && isOn) || bottomTemperature < 32.5)
+  {
+    digitalWrite(RELAY, LOW);
+    isOn = false;
+  }
 
 }
 
@@ -107,36 +164,35 @@ void cycleDisplay()
 
 void loop()
 {
-  sensors.requestTemperatures();
+  static int pos = 0;
 
-  topTemperature = sensors.getTempF(topThermometer);
-  bottomTemperature = sensors.getTempF(bottomThermometer);
-  avgTemp = topTemperature + bottomTemperature;
-  avgTemp /= 2;
-  
-  
-  int aread = analogRead(POT);
-  target = map_25inc(aread, 0, 1023, LOW_END, HIGH_END);
-  
-  if(avgTemp > target + DELTA_T && !isOn)
+  encoder.tick();
+  int newPos = encoder.getPosition();
+  if(pos != newPos)
   {
-    digitalWrite(RELAY, HIGH);
-    isOn = true;
+#if DEBUG
+    Serial.println(newPos);
+    pos = newPos;
+    Serial.println(pos);
+    float offTemp = ((float)newPos / 4.0);
+    Serial.println(offTemp);
+    target = DEFAULT_TEMP + offTemp;
+    Serial.println(target);
+#else
+    pos = newPos;
+    float offTemp = ((float)newPos / 4.0);
+    target = DEFAULT_TEMP + offTemp;
+#endif
   }
 
-  if(avgTemp <= target - DELTA_T && isOn)
-  {
-    digitalWrite(RELAY, LOW);
-    isOn = false;
-  }
 
   if(displayCycle.shouldRun())
     displayCycle.run();
+
+  if(tempReadThread.shouldRun())
+    tempReadThread.run();
+
+  if(updateSecondLine.shouldRun())
+    updateSecondLine.run();
   
-  lcd.setCursor(0,1);
-  lcd.print("Target: ");
-  lcd.print(target);
-  lcd.print(" F");
-  
-  delay(20);
 }
